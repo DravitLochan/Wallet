7e46f068fb3f769543b108dc8e58f1f1498ae434
Naman Arora <naman.arora11@gmail.com>
2016-10-11 17:36:33 +0530

    Updated user screen
    -Added graph api for future use
    -Removed unecessary fields
    -Added a layout for showing current stats

--- app/src/main/java/naman/com/wallet/User.java
+++ app/src/main/java/naman/com/wallet/User.java
@@ -2,6 +2,9 @@ package naman.com.wallet;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,6 +19,11 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
 public class User extends Fragment {
     @Nullable
     @Override
@@ -24,6 +32,20 @@ public class User extends Fragment {
         context=super.getActivity();
         initUI(root);
 
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.setTitle("Money Manager");
+        series.setColor(Color.GREEN);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(10);
+        series.setThickness(8);
+        graph.addSeries(series);
+
         return root;
     }
 
@@ -31,15 +53,11 @@ public class User extends Fragment {
     Button logout;
     SessionManager manager;
     Context context;
-
+    GraphView graph;
 
     private void initUI(View root) {
-
+        graph = (GraphView) root.findViewById(R.id.graph);
         manager= new SessionManager(context);
-        email=(TextView)root.findViewById(R.id.emailid);
-        name=(TextView)root.findViewById(R.id.name);
-        email.setText(manager.getPreferences("email"));
-        name.setText(manager.getPreferences("name"));
         logout=(Button)root.findViewById(R.id.button_logout) ;
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
